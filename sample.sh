a7cfe118656a94032a2792c82207d93ddd11b391
root <root@ip-172-31-2-60.ap-south-1.compute.internal>
2024-12-07 14:21:15 +0000

    first commit

---
+++ sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Executing the bash script
+echo ##################################
+
+echo "execute the system uptime command : "
+uptime
+
+echo #################################
+echo "memory utilization"
+df-h
+
+
+echo ################################
+echo "space utilization"
+free -m
